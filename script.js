f71028e382f4e316f920fcfe39af7eae308a74e9
sgutsch-hartje <stanislaw.gutsch@hartje.de>
2026-02-25 14:31:17 +0100

    cleanup

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('kebabForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    const bread = document.querySelector('select[name="bread"]').value;
-    const meat = document.querySelector('select[name="meat"]').value;
-    const salad = document.querySelector('select[name="salad"]').value;
-    const summary = document.getElementById('orderSummary');
-    summary.innerHTML = `Your order: <br>Bread: ${bread}<br>Meat: ${meat}<br>Salad: ${salad}`;
-});
